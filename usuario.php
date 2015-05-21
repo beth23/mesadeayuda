a01225ce6c085d27478a2d5a7a26a89a79299e08
PaulSiA <leng.floyd@gmail.com>
2015-05-20 21:26:35 -0500

    Consultas anteriores usuario php y tabla

--- usuario.php
+++ usuario.php
@@ -81,6 +81,45 @@ alert("tu consulta fue enviada ")
 <div id="marco1">
 <br />
 <h2>Consultas anteriores </h2>
+<?php
+
+/* Abrimos la base de datos */
+  $conx = mysql_connect ("localhost","root","");
+  if (!$conx) die ("Error al abrir la base <br/>". mysql_error()); 
+  mysql_select_db("test") OR die("Connection Error to Database");    
+
+/* Realizamos la consulta SQL */
+$sql="select * from agenda";
+$result= mysql_query($sql) or die(mysql_error());
+if(mysql_num_rows($result)==0) die("No hay registros para mostrar");
+
+/* Desplegamos cada uno de los registros dentro de una tabla */  
+echo "<table border=1 cellpadding=4 cellspacing=0>";
+
+/*Priemro los encabezados*/
+ echo "<tr>
+         <th colspan=5> Consultas </th>
+       <tr>
+         <th> IDc </th><th>fecha  </th><th>&Aacute;rea  </th>
+         <th> Especialidad</th><th> Descripcion</th>
+      </tr>";
+
+/*Y ahora todos los registros */
+while($row=mysql_fetch_array($result))
+{
+ echo "<tr>
+         <td align='right'> $row[idc] </td>
+         <td> $row[fecha] </td>
+         <td> $row[Area] </td>
+         <td> $row[especialidad] </td>
+         <td> $row[Descripcion] </td>
+      </tr>";
+}
+echo "</table>";
+
+?>
+
+
 </div> 
  
 <div id="marco2">
