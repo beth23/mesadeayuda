6df61f7c4543bef15472f8b595da3d756a3b78ee
PaulSiA <leng.floyd@gmail.com>
2015-05-20 21:27:54 -0500

    lo mismo de la anterior

--- usuario.php
+++ usuario.php
@@ -83,20 +83,20 @@ alert("tu consulta fue enviada ")
 <h2>Consultas anteriores </h2>
 <?php
 
-/* Abrimos la base de datos */
+
   $conx = mysql_connect ("localhost","root","");
   if (!$conx) die ("Error al abrir la base <br/>". mysql_error()); 
   mysql_select_db("test") OR die("Connection Error to Database");    
 
-/* Realizamos la consulta SQL */
-$sql="select * from agenda";
+
+$sql="select * from tabla";
 $result= mysql_query($sql) or die(mysql_error());
 if(mysql_num_rows($result)==0) die("No hay registros para mostrar");
 
-/* Desplegamos cada uno de los registros dentro de una tabla */  
+
 echo "<table border=1 cellpadding=4 cellspacing=0>";
 
-/*Priemro los encabezados*/
+
  echo "<tr>
          <th colspan=5> Consultas </th>
        <tr>
@@ -104,7 +104,7 @@ echo "<table border=1 cellpadding=4 cellspacing=0>";
          <th> Especialidad</th><th> Descripcion</th>
       </tr>";
 
-/*Y ahora todos los registros */
+
 while($row=mysql_fetch_array($result))
 {
  echo "<tr>
